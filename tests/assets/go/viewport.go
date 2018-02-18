23ec92cf847ae19385e59e5a5079e2095018ac3d
selimanac <selimanac@gmail.com>
2018-02-18 09:50:00 +0300

    0.7

--- tests/assets/go/viewport.go
+++ tests/assets/go/viewport.go
@@ -1,7 +1,7 @@
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/assets/images/test.atlas\"\n"
+  data: "tile_set: \"/tests/assets/images/test.atlas\"\n"
   "default_animation: \"view\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
